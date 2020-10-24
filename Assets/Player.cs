43af9aa00b9820568aa09f7d2897064dcc7ad4b3
Einar Budsted <ivazvaka@hotmail.com>
2020-10-25 00:36:27 +0200

    death counter

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public int deathCount = 0;
     public Vector2 respawnPoint = new Vector2(0f, 0f);
+    public TextMeshProUGUI deathsText;
 
     public void KillPlayer()
     {
         deathCount += 1;
         transform.position = new Vector3(respawnPoint.x, respawnPoint.y, 0f);
+        deathsText.text = "Deaths: " + deathCount.ToString();
     }
 }
