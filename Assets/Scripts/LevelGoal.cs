f6cbfedc7d97ee6957c773003ca6a020d2cf3069
Einar Budsted <ivazvaka@hotmail.com>
2020-11-02 09:28:15 +0100

    forgot to commit and push this which was part of the finished build

--- Assets/Scripts/LevelGoal.cs
+++ Assets/Scripts/LevelGoal.cs
@@ -13,7 +13,7 @@ public class LevelGoal : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (entered == false) {
+        if (entered == false && collision.gameObject.GetComponent<Player>()) {
             entered = true;
             collision.gameObject.GetComponent<PlayerMovement>().isControllsLocked = true;
             StartCoroutine(Accend());
